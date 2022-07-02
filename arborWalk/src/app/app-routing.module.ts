3d73300e2f0b7776713af0e36635ac7a3fb1c633
Sayed <sayed.musavi@turner.com>
2022-07-02 18:35:15 -0400

    make it flaxible to small screens

--- arborWalk/src/app/app-routing.module.ts
+++ arborWalk/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 import { ProfileComponent } from './profile/profile.component';
 import { SignInComponent } from './sign-in/sign-in.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
@@ -8,7 +9,11 @@ const routes: Routes = [
   {
     path: '',
     redirectTo: 'home',
-    pathMatch: 'full',
+    pathMatch: 'prefix',
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
   },
   {
     path: 'profile',
@@ -24,7 +29,7 @@ const routes: Routes = [
   },
   {
     path: '**',
-    redirectTo: 'home',
+    redirectTo: '',
   },
 ];
 
